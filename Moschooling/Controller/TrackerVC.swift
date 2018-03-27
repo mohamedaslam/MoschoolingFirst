fe2ddb1af53c698dffe5c0d2287dff8d3bf968bd
Mohammed Aslam <maslam@oottru.com>
2018-03-27 19:12:43 +0530

    Dashboard Basic UI

---
+++ Moschooling/Controller/TrackerVC.swift
@@ -0,0 +1,36 @@
+//
+//  TrackerVC.swift
+//  Moschooling
+//
+//  Created by Mohammed Aslam on 27/03/18.
+//  Copyright © 2018 Moschooling. All rights reserved.
+//
+
+import UIKit
+
+class TrackerVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSlideMenuButton()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
